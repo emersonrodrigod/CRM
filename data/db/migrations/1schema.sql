1de5cfb313f75af42d704d1ee3464a0dc98c7337
emersonrodrigod <emersonrodrigod@gmail.com>
2014-06-05 16:16:13 -0300

    Migration Schema atual

---
+++ data/db/migrations/1schema.sql
@@ -0,0 +1,279 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Máquina: localhost
+-- Data de Criação: 05-Jun-2014 às 19:15
+-- Versão do servidor: 5.6.12-log
+-- versão do PHP: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `intranet`
+--
+CREATE DATABASE IF NOT EXISTS `intranet` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `intranet`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `anexo`
+--
+
+CREATE TABLE IF NOT EXISTS `anexo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `caminho` varchar(255) NOT NULL,
+  `id_historico` int(11) DEFAULT NULL,
+  `id_tarefa` int(11) DEFAULT NULL,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_historico` (`id_historico`),
+  KEY `id_tarefa` (`id_tarefa`),
+  KEY `id_usuario` (`id_usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `ativo` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(10) DEFAULT NULL,
+  `nome` varchar(255) NOT NULL,
+  `estado` char(2) DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `cnpj` varchar(19) DEFAULT NULL,
+  `descricao` text,
+  `privacidade` int(11) NOT NULL DEFAULT '0',
+  `id_responsavel` int(11) DEFAULT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_responsavel` (`id_responsavel`),
+  KEY `id_categoria` (`id_categoria`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2446 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `departamento`
+--
+
+CREATE TABLE IF NOT EXISTS `departamento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(155) NOT NULL,
+  `ativo` int(11) NOT NULL DEFAULT '1',
+  `id_empresa` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_departamento_empresa_idx` (`id_empresa`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `empresa`
+--
+
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `ativo` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `historico`
+--
+
+CREATE TABLE IF NOT EXISTS `historico` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `texto` text NOT NULL,
+  `quando` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tarefa` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_usuario` (`id_usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=90 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `quando` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_usuario` (`id_usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `privacidade_historico`
+--
+
+CREATE TABLE IF NOT EXISTS `privacidade_historico` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_cliente` (`id_cliente`,`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tarefa`
+--
+
+CREATE TABLE IF NOT EXISTS `tarefa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `texto` text NOT NULL,
+  `dtTarefa` date NOT NULL,
+  `horaTarefa` time NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `conclusao` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `id_historico` int(11) DEFAULT NULL,
+  `situacao` char(3) NOT NULL DEFAULT 'PEN',
+  PRIMARY KEY (`id`),
+  KEY `id_usuario` (`id_usuario`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_historico` (`id_historico`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tarefa_usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `tarefa_usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL,
+  `id_tarefa` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_usuario` (`id_usuario`,`id_tarefa`),
+  KEY `id_tarefa` (`id_tarefa`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `ativo` int(11) NOT NULL DEFAULT '1',
+  `dataCadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` varchar(45) NOT NULL,
+  `id_empresa` int(11) DEFAULT NULL,
+  `id_departamento` int(11) DEFAULT NULL,
+  `telefone` varchar(14) DEFAULT NULL,
+  `ramal` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_usuario_empresa_idx` (`id_empresa`),
+  KEY `fk_usuario_departamento_idx` (`id_departamento`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `anexo`
+--
+ALTER TABLE `anexo`
+  ADD CONSTRAINT `anexo_ibfk_1` FOREIGN KEY (`id_historico`) REFERENCES `historico` (`id`),
+  ADD CONSTRAINT `anexo_ibfk_2` FOREIGN KEY (`id_tarefa`) REFERENCES `tarefa` (`id`),
+  ADD CONSTRAINT `anexo_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Limitadores para a tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_responsavel`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `cliente_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`);
+
+--
+-- Limitadores para a tabela `departamento`
+--
+ALTER TABLE `departamento`
+  ADD CONSTRAINT `fk_departamento_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `historico`
+--
+ALTER TABLE `historico`
+  ADD CONSTRAINT `historico_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `historico_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Limitadores para a tabela `migrations`
+--
+ALTER TABLE `migrations`
+  ADD CONSTRAINT `migrations_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Limitadores para a tabela `tarefa`
+--
+ALTER TABLE `tarefa`
+  ADD CONSTRAINT `tarefa_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `tarefa_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `tarefa_ibfk_3` FOREIGN KEY (`id_historico`) REFERENCES `historico` (`id`);
+
+--
+-- Limitadores para a tabela `tarefa_usuario`
+--
+ALTER TABLE `tarefa_usuario`
+  ADD CONSTRAINT `tarefa_usuario_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `tarefa_usuario_ibfk_2` FOREIGN KEY (`id_tarefa`) REFERENCES `tarefa` (`id`);
+
+--
+-- Limitadores para a tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_usuario_departamento` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_usuario_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
